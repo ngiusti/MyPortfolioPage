4c8b21bcdad0109e2efbe67fe2f38d7c7621d6e4
Nicholas Giusti <nicholas.giusti89@gmail.com>
2017-09-28 15:26:22 -0700

    Updated work and projects

---
+++ MyPortfolioPage/PHP/send.php
@@ -0,0 +1,14 @@
+﻿<?php
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$message = $_POST['message'];
+
+$to = "nicholas.giusti89@gmail.com";
+$subject = "Webpage Email";
+$body = "This is an automated message \n\n $message";
+
+mail ($to,$subject,$body);
+
+
+?>
